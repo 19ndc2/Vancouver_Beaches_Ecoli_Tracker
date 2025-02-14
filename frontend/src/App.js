378201bd1a50f881e9a7f80a879dc34f76ad1785
19ndc2 <19ndc2@queensu.ca>
2025-02-13 22:36:59 -0500

    Added a spinner while waiting for response from backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,15 @@
 import React, { useState, useEffect } from "react";
 import './App.css';
 import MapComponent from "./MapComponent";
+import LoadingSpinner from "./LoadingSpinner";
 
 function App() {
   const [beaches, setBeaches] = useState([]);
-
+  const [loading, setLoading] = useState(true);
+  
   //get beach data
   useEffect(() => {
+    setLoading(true); //start loading
     fetch("https://vancouver-beaches-ecoli-tracker.onrender.com/beach_ecoli_data")
     //fetch("http://127.0.0.1:5000/beach_ecoli_data")
       .then((res) => res.json())
@@ -15,7 +18,11 @@ function App() {
         setBeaches(data);  
 
       })
-      .catch((error) => console.error("Error fetching data:", error));
+      .catch((error) => console.error("Error fetching data:", error))
+      .finally(() => {
+        setLoading(false); //stop loading
+      }
+      );
   }, []);
 
 
@@ -23,34 +30,44 @@ function App() {
   useEffect(() => {
     console.log("Beaches State Updated:", beaches);
   }, [beaches]);
+  
+ 
 
   return (
     <div className="container">
         <h1>Vancouver Beaches E. coli Tracker</h1>
-        <div style={{ width: "100%", maxWidth: "1000px", margin: "auto" }}>
-          <MapComponent beaches={beaches} />
-        </div>
 
-        <table>
-        <thead>
-          <tr>
-              <th>Beach</th>              
-              <th>Status</th>
-              <th>E. coli Level MPN/100mL</th>
-              <th>Sample date</th>
-          </tr>
-        </thead>
-        <tbody>
-          {beaches && beaches.map((beach, index) => (
-              <tr key={index}>
-                <td>{beach.name}</td>
-                <td className={beach.status}>{beach.status}</td>                 
-                <td>{beach.ecoli_level}</td>
-                <td>{beach.sample_date}</td>
-              </tr>
-            ))}
-        </tbody>
-      </table>
+        {/*Show loading spinner while waiting for backend response */}
+        {loading ? (
+          <LoadingSpinner />    
+        ) : (
+          
+          <>
+          <div style={{ width: "100%", maxWidth: "1000px", margin: "auto" }}>
+            <MapComponent beaches={beaches} />
+          </div>
+
+          <table>
+          <thead>
+            <tr>
+                <th>Beach</th>              
+                <th>Status</th>
+                <th>E. coli Level MPN/100mL</th>
+                <th>Sample date</th>
+            </tr>
+          </thead>
+          <tbody>
+            {beaches && beaches.map((beach, index) => (
+                <tr key={index}>
+                  <td>{beach.name}</td>
+                  <td className={beach.status}>{beach.status}</td>                 
+                  <td>{beach.ecoli_level}</td>
+                  <td>{beach.sample_date}</td>
+                </tr>
+              ))}
+          </tbody>
+        </table>
+        </>)}
     </div>
   );
 }
